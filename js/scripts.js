ef6aafbebcc6daa71ac83f031ba348962486d55c
Laosing <andrew.rattana@gmail.com>
2015-04-27 23:57:27 -0400

    Merging gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,5 @@
 
     $(window).resize();
 
-    $('.learn-more').click(function(event) {
-      event.preventDefault();
-      var offsetTop = $('#about').offset().top - 70;
-      $(window).scrollTo(offsetTop, 1000, 'ease-in-out');
-    });
-
   });
 })(jQuery, this, this.document);
